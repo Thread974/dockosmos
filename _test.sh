3b4d9879637e65f448901c6052be9d36016e6fe0
Frédéric Dalleau <fred@dalleau.re>
2016-12-07 06:26:02 +0100

    Use volumes download and database locations

--- _test.sh
+++ _test.sh
@@ -1,12 +1,33 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
+
+# Use full path for volumes
+ROOTPATH=$1
+[ -z "$ROOTPATH" ] && ROOTPATH=$(pwd)
+
+# Path on the host
+DB=$ROOTPATH/volumedb
+DL=$ROOTPATH/volumedl
+
+# Mount path in the container
+DBMOUNT=/var/lib/postgresql/data
+DLMOUNT=/dl
+OSM_ROOT_DIR=$DL
+
+mkdir -p $DB
+mkdir -p $DL
 
 docker kill osmos || echo "Not running"
 docker rm -v osmos || echo "Not container"
 docker rmi dockosmos || echo "No image"
 docker build -t dockosmos .
-docker run -d --name osmos dockosmos
 
-time docker exec osmos /root/dl.sh dl-europe import select
+OPTIONS=" -e OSM_ROOT_DIR=$DLMOUNT"
+OPTIONS+=" -v $DB:$DBMOUNT"
+OPTIONS+=" -v $DL:$DLMOUNT"
+OPTIONS+=" -d --name osmos"
+docker run $OPTIONS dockosmos
+
+time docker exec osmos /root/dl.sh dl import select
 
